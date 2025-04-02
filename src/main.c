b00da5af82595e6d3c7f4ae42362c22e4813dca2
Shoko Soeno <ssoeno@c6r1s10.42tokyo.jp>
2025-04-02 18:45:06 +0900

    resolve coflicts

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ssoeno <ssoeno@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 17:01:34 by ssoeno            #+#    #+#             */
-/*   Updated: 2025/04/01 17:01:45 by ssoeno           ###   ########.fr       */
+/*   Updated: 2025/04/02 18:44:31 by ssoeno           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,8 @@ int main(int argc, char **argv)
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
 	// scene初期化
-	// ft_bzero(&data, sizeof(t_data));
-	// if (parse_rt_file(argv[1], &data.scene) != 0)
+	// ft_bzero(&data, sizeof(t_scene));
+	// if (parse_rt_file(argv[1], &scebe.scene) != 0)
 	// 	return (1);
 	// render_scene(&data);
 	mlx_hook(mlx_win, 2, 1L << 0, key_event, NULL);
