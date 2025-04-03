abad38af4087ee5ff12252055bbc336961eabcf6
yuu-ux <yuugaehara@gmail.com>
2025-04-03 19:52:44 +0900

    :wrench: 重複のエラー処理追加

--- src/parse/parse_ambient.c
+++ src/parse/parse_ambient.c
@@ -6,18 +6,21 @@
 /*   By: yehara <yehara@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 16:56:00 by yehara            #+#    #+#             */
-/*   Updated: 2025/04/03 19:26:14 by yehara           ###   ########.fr       */
+/*   Updated: 2025/04/03 19:42:01 by yehara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <scene.h>
 #include <parse.h>
+#include <scene.h>
 
 int	parse_ambient(char **elements, t_ambient *ambient)
 {
-	char **colors;
-	double brightness;
+	char	**colors;
+	double	brightness;
 
+	if (!((ambient->brightness == -1) || (ambient->color.r == -1)
+			|| (ambient->color.g == -1) || (ambient->color.b == -1)))
+		return (EXIT_FAILURE);
 	colors = ft_split(elements[1], ',');
 	if (count_array(elements) > 2)
 		return (EXIT_FAILURE);
@@ -29,4 +32,3 @@ int	parse_ambient(char **elements, t_ambient *ambient)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
-
