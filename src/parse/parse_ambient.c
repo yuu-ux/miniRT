5d39f9d8dd1b93b18435825942191be86959176b
yuu-ux <yuugaehara@gmail.com>
2025-04-03 22:16:53 +0900

    :wrench: 条件のミスを修正

--- src/parse/parse_ambient.c
+++ src/parse/parse_ambient.c
@@ -15,20 +15,18 @@
 
 int	parse_ambient(char **elements, t_ambient *ambient)
 {
-	char	**colors;
 	double	brightness;
 
-	if (!((ambient->brightness == -1) || (ambient->color.r == -1)
-			|| (ambient->color.g == -1) || (ambient->color.b == -1)))
+	if (ambient->brightness != -1 || ambient->color.r != -1
+		|| ambient->color.g != -1 || ambient->color.b != -1)
 		return (EXIT_FAILURE);
-	colors = ft_split(elements[1], ',');
 	if (count_array(elements) > 2)
 		return (EXIT_FAILURE);
 	brightness = ft_atof(elements[0]);
-	if (brightness < 0.0 && brightness > 1.0)
+	if (brightness < 0.0 || brightness > 1.0)
 		return (EXIT_FAILURE);
-	ambient->brightness = ft_atof(elements[0]);
-	if (parse_color(colors, &ambient->color) == EXIT_FAILURE)
+	ambient->brightness = brightness;
+	if (parse_color(elements[1], &ambient->color) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
