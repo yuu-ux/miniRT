bea71464f2ec7442dfcd3252e43c0d5e27f26977
yuu-ux <yuugaehara@gmail.com>
2025-04-05 19:42:49 +0900

    :wrench: light テスト追加、ディレクトリ構成を変更

--- test/parse/main.c
+++ test/parse/main.c
@@ -6,19 +6,26 @@ void	init_data(t_scene *scene)
 	scene->ambient.color.r = -1;
 	scene->ambient.color.g = -1;
 	scene->ambient.color.b = -1;
-    scene->camera.position.x = -1;
-    scene->camera.position.y = -1;
-    scene->camera.position.z = -1;
-    scene->camera.orientation.x = -1;
-    scene->camera.orientation.y = -1;
-    scene->camera.orientation.z = -1;
-    scene->camera.fov = -1;
+	scene->camera.position.x = -1;
+	scene->camera.position.y = -1;
+	scene->camera.position.z = -1;
+	scene->camera.orientation.x = -1;
+	scene->camera.orientation.y = -1;
+	scene->camera.orientation.z = -1;
+	scene->camera.fov = -1;
+	scene->light.position.x = -1;
+	scene->light.position.y = -1;
+	scene->light.position.z = -1;
+	scene->light.brightness = -1;
+	scene->light.color.r = -1;
+	scene->light.color.g = -1;
+	scene->light.color.b = -1;
 }
 
 int	main(int argc, char **argv)
 {
 	t_scene	*scene;
-    int status;
+	int		status;
 
 	if (argc != 2)
 	{
@@ -27,20 +34,27 @@ int	main(int argc, char **argv)
 	}
 	scene = ft_xmalloc(sizeof(t_scene));
 	init_data(scene);
-    status  = parse_rt_file(argv[1], scene);
-    if (status == EXIT_SUCCESS)
-    {
-        printf("brighteness: %f\n", scene->ambient.brightness);
-        printf("red: %f\n", scene->ambient.color.r);
-        printf("green: %f\n", scene->ambient.color.g);
-        printf("blue: %f\n", scene->ambient.color.b);
-        printf("x: %f\n", scene->camera.position.x);
-        printf("y: %f\n", scene->camera.position.y);
-        printf("z: %f\n", scene->camera.position.z);
-        printf("x: %f\n", scene->camera.orientation.x);
-        printf("y: %f\n", scene->camera.orientation.y);
-        printf("z: %f\n", scene->camera.orientation.z);
-        printf("fov: %f\n", scene->camera.fov);
-    }
+	status = parse_rt_file(argv[1], scene);
+	if (status == EXIT_SUCCESS)
+	{
+		printf("brightness: %f\n", scene->ambient.brightness);
+		printf("red: %f\n", scene->ambient.color.r);
+		printf("green: %f\n", scene->ambient.color.g);
+		printf("blue: %f\n", scene->ambient.color.b);
+		printf("x: %f\n", scene->camera.position.x);
+		printf("y: %f\n", scene->camera.position.y);
+		printf("z: %f\n", scene->camera.position.z);
+		printf("x: %f\n", scene->camera.orientation.x);
+		printf("y: %f\n", scene->camera.orientation.y);
+		printf("z: %f\n", scene->camera.orientation.z);
+		printf("fov: %f\n", scene->camera.fov);
+		printf("x: %f\n", scene->light.position.x);
+		printf("y: %f\n", scene->light.position.y);
+		printf("z: %f\n", scene->light.position.z);
+		printf("brightness: %f\n", scene->light.brightness);
+		printf("red: %f\n", scene->light.color.r);
+		printf("green: %f\n", scene->light.color.g);
+		printf("blue: %f\n", scene->light.color.b);
+	}
 	return (status);
 }
