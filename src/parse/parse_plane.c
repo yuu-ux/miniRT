3ce603bc1abe60fe3226dd19b3679389289c2311
yuu-ux <yuugaehara@gmail.com>
2025-04-07 20:34:17 +0900

    :bug: 書き込み先修正

--- src/parse/parse_plane.c
+++ src/parse/parse_plane.c
@@ -6,29 +6,28 @@
 /*   By: yehara <yehara@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 18:01:11 by yehara            #+#    #+#             */
-/*   Updated: 2025/04/07 18:02:05 by yehara           ###   ########.fr       */
+/*   Updated: 2025/04/07 20:33:37 by yehara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <scene.h>
 #include <parse.h>
+#include <scene.h>
 
-t_object    *parse_plane(char **elements)
+t_object	*parse_plane(char **elements)
 {
-	t_object *plane;
+	t_object	*plane;
 
 	plane = ft_xmalloc(sizeof(t_object));
 	if (count_array(elements) != 3)
 		return (NULL);
 	if (parse_vector(elements[0], &plane->center) == EXIT_FAILURE)
 		return (NULL);
-	if (parse_vector(elements[1], &plane->center) == EXIT_FAILURE)
+	if (parse_vector(elements[1], &plane->normal) == EXIT_FAILURE)
 		return (NULL);
-	if (validate_vector(plane->center) == EXIT_FAILURE)
+	if (validate_vector(plane->normal) == EXIT_FAILURE)
 		return (NULL);
 	if (parse_color(elements[2], &plane->color) == EXIT_FAILURE)
 		return (NULL);
 	plane->shape = PLANE;
 	return (plane);
 }
-
