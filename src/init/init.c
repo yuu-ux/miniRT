a23b22028957459f3fcbe89589337e0eb25cbbbd
yuu-ux <yuugaehara@gmail.com>
2025-04-14 19:15:54 +0900

    :space_invader: 初期化できてなかったため、修正

--- src/init/init.c
+++ src/init/init.c
@@ -6,22 +6,41 @@
 /*   By: yehara <yehara@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 20:04:28 by yehara            #+#    #+#             */
-/*   Updated: 2025/04/07 20:14:22 by yehara           ###   ########.fr       */
+/*   Updated: 2025/04/14 19:15:07 by yehara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <scene.h>
 #include <mlx_utils.h>
+#include <scene.h>
 
 #define WIDTH 800
 #define HEIGHT 600
+#define INITIAL_VALUE -1
 
-static void init_scene(t_scene *scene)
+static void	init_scene(t_scene *scene)
 {
-	ft_memset(scene, -1, sizeof(t_scene));
+	scene->ambient.brightness = -1;
+	scene->ambient.color.r = -1;
+	scene->ambient.color.g = -1;
+	scene->ambient.color.b = -1;
+	scene->camera.position.x = -1;
+	scene->camera.position.y = -1;
+	scene->camera.position.z = -1;
+	scene->camera.orientation.x = -1;
+	scene->camera.orientation.y = -1;
+	scene->camera.orientation.z = -1;
+	scene->camera.fov = -1;
+	scene->light.position.x = -1;
+	scene->light.position.y = -1;
+	scene->light.position.z = -1;
+	scene->light.brightness = -1;
+	scene->light.color.r = -1;
+	scene->light.color.g = -1;
+	scene->light.color.b = -1;
+	scene->objects = NULL;
 }
 
-static void init_mlx(t_mlx *mlx)
+static void	init_mlx(t_mlx *mlx)
 {
 	mlx->mlx = mlx_init();
 	mlx->window = mlx_new_window(mlx->mlx, WIDTH, HEIGHT, "miniRT");
@@ -37,4 +56,3 @@ void	init_data(t_scene *scene, t_mlx *mlx)
 	init_scene(scene);
 	init_mlx(mlx);
 }
-
