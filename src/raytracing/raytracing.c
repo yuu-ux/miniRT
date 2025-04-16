e248e2ef4a11ab91e6affaf9daf870ddc13d6ecc
yuu-ux <yuugaehara@gmail.com>
2025-04-16 21:55:04 +0900

    :construction: 赤い丸が描画された

--- src/raytracing/raytracing.c
+++ src/raytracing/raytracing.c
@@ -10,28 +10,68 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "scene.h"
 #include <mlx_util.h>
 
+double	map(double num, double new_min, double new_max, double old_max)
+{
+	return (((new_max - new_min) * num / old_max) + new_min);
+}
+
+int	hit_sphere(t_vec center, double radius, t_vec origin, t_vec dir)
+{
+	t_vec	oc;
+	double	a;
+	double	b;
+	double	c;
+	double	discriminant;
+
+	oc = subtract(origin, center);
+	a = dot_product(dir, dir);
+	b = 2.0 * dot_product(oc, dir);
+	c = dot_product(oc, oc) - radius * radius;
+	discriminant = b * b - 4 * a * c;
+	return (discriminant >= 0);
+}
+
+static void	ft_pixel_put(int x, int y, t_img *img, int color)
+{
+	size_t	offset;
+
+	offset = (y * img->line_length) + x * (img->bpp / 8);
+	*(unsigned int *)(img->addr + offset) = color;
+}
 
 void	raytracing(t_mlx *mlx)
 {
-	t_color pixel_color;
-	t_vec ray_direction;
-	int	x;
-	int y;
+	t_vec		dir;
+	t_vec		ray_origin;
+	t_object	*sphere;
+	double		screen_z;
+	double		ndc_x;
+	double		ndc_y;
 
+	int x, y;
+	ray_origin = mlx->scene.camera.orientation;
+	sphere = (t_object *)mlx->scene.objects->content;
+	screen_z = 1.0;
 	y = 0;
 	while (y < mlx->img.height)
 	{
 		x = 0;
 		while (x < mlx->img.width)
 		{
-			// 例の方向を求める
-			ray_direction = calculate_ray_direction(*mlx, x, y);
-			// ピクセルの色を求める
-			pixel_color = trace_ray(mlx, mlx->scene->camera.position, ray_direction);
-			// ピクセルの色をセットする
-			set_pixel_color(&mlx->img, x, y, pixel_color);
+			// スクリーン上のピクセルをNDC → スクリーン座標へ変換
+			ndc_x = (2.0 * x / (double)(mlx->img.width - 1)) - 1.0;
+			ndc_y = 1.0 - (2.0 * y / (double)(mlx->img.height - 1));
+			// レイ方向を作る
+			dir = normalize((t_vec){ndc_x, ndc_y, screen_z});
+			// 衝突判定（球との交差）
+			if (hit_sphere(sphere->center, sphere->diameter / 2.0, ray_origin,
+					dir))
+				ft_pixel_put(x, y, &mlx->img, 0xFF0000); // 赤で描画
+			else
+				ft_pixel_put(x, y, &mlx->img, 0x000000); // 黒で背景
 			x++;
 		}
 		y++;
