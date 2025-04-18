1b9882b61fb50f864f2e0bd50169c74f4cb8bb64
yuu-ux <yuugaehara@gmail.com>
2025-04-18 20:04:48 +0900

    :bug: 10..0のようなケースに対応できてなかったため修正

--- src/util/ft_xatof.c
+++ src/util/ft_xatof.c
@@ -15,13 +15,24 @@
 
 static bool	is_conversion_float(char *s)
 {
+	int	dot_count;
+	int	digit_count;
+
+	dot_count = 0;
+	digit_count = 0;
+	if (*s == '+' || *s == '-')
+		s++;
 	while (*s)
 	{
-		if (!(ft_isdigit(*s) || (*s == '.') || (*s == '+') || (*s == '-')))
+		if (*s == '.')
+			dot_count++;
+		else if (ft_isdigit(*s))
+			digit_count++;
+		else
 			return (false);
 		s++;
 	}
-	return (true);
+	return (dot_count <= 1 && digit_count > 0);
 }
 
 double	ft_xatof(char *s)
