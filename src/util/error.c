4d615fd5bc5a3dd300a13d933bf26e8d89fd5245
yuu-ux <yuugaehara@gmail.com>
2025-04-27 11:38:11 +0900

    :lipstick: 文言変更

--- src/util/error.c
+++ src/util/error.c
@@ -10,10 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <scene.h>
 #include <parse.h>
+#include <scene.h>
 #include <util.h>
-
 
 int	free_double_array(char **array)
 {
@@ -26,12 +25,12 @@ int	free_double_array(char **array)
 	return (EXIT_FAILURE);
 }
 
-static char *get_error_message(int status)
+static char	*get_error_message(int status)
 {
 	if (status == ERR_DUPLICATE)
 		return ("要素が重複してます。");
 	else if (status == ERR_ARG_COUNT)
-		return ("引数の数が不適切");
+		return ("引数の数が不適切です。");
 	else if (status == ERR_BRIGHTNESS_RANGE)
 		return ("輝度が有効範囲外です。");
 	else if (status == ERR_COLOR_FORMAT)
@@ -45,7 +44,7 @@ static char *get_error_message(int status)
 	return ("不明なエラーが発生しました。");
 }
 
-int	error_exit(char *message, int	status)
+int	error_exit(char *message, int status)
 {
 	if (status != INVALID_ERR_STATUS)
 		message = get_error_message(status);
@@ -54,4 +53,3 @@ int	error_exit(char *message, int	status)
 	ft_putstr_fd("\n", 2);
 	exit(EXIT_FAILURE);
 }
-
