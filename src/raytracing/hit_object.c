dae7b474b08dc737f85487bf56336c78ce05e830
Shoko Soeno <ssoeno@c5r3s2.42tokyo.jp>
2025-04-27 19:39:42 +0900

    add hit_cylinder

--- src/raytracing/hit_object.c
+++ src/raytracing/hit_object.c
@@ -6,7 +6,7 @@
 /*   By: ssoeno <ssoeno@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 15:45:50 by ssoeno            #+#    #+#             */
-/*   Updated: 2025/04/27 19:11:21 by ssoeno           ###   ########.fr       */
+/*   Updated: 2025/04/27 19:38:48 by ssoeno           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,10 +65,34 @@ dir: rayの進行方向ベクトル
 denominator: normal vectorとrayの方向が平行に近い = 内積が正 = rayがplaneの裏側から来ている
 */
 
-double hit_cylinder(t_vec center, double radius, t_vec origin, t_vec dir)
+double hit_cylinder(t_vec center, double radius, t_vec normal, t_vec origin, t_vec dir)
 {
-    // Implement cylinder intersection logic here
-    return (-1.0);
+    t_vec	origin_to_center;
+    t_vec	ray_dir_perpendicular;
+    t_vec   origin_perpendicular;
+    double	a;
+    double	b;
+    double	c;
+    double	discriminant;
+    double	t;
+    
+    origin_to_center = subtract(origin, center);
+    ray_dir_perpendicular = subtract(dir, scale(normal, dot_product(dir, normal)));
+    origin_perpendicular = subtract(origin_to_center, scale(normal, dot_product(origin_to_center, normal)));
+    a = dot_product(ray_dir_perpendicular, ray_dir_perpendicular);
+    b = 2.0 * dot_product(ray_dir_perpendicular, origin_perpendicular);
+    c = dot_product(origin_perpendicular, origin_perpendicular) - radius * radius;
+    discriminant = b * b - 4 * a * c;
+    if (discriminant < 0)
+        return (-1.0);
+    t = (-b - sqrt(discriminant)) / (2.0 * a);
+    if (t < 1e-4)
+    {
+        t = (-b + sqrt(discriminant)) / (2.0 * a);
+        if (t < 1e-4)
+            return (-1.0);
+    }
+    return (t);
 }
 
 double hit_object(t_vec origin, t_vec dir, t_object *obj)
@@ -77,7 +101,7 @@ double hit_object(t_vec origin, t_vec dir, t_object *obj)
 		return (hit_sphere(obj->center, obj->diameter / 2.0, origin, dir));
 	else if (obj->shape == PLANE)
 		return (hit_plane(obj->center, obj->normal, origin, dir));
-	// else if (obj->shape == CYLINDER)
-	// 	return (hit_cylinder(obj, origin, dir));
+	else if (obj->shape == CYLINDER)
+		return (hit_cylinder(obj->center, obj->diameter / 2.0, obj->normal, origin, dir));
 	return (-1.0);
 }
