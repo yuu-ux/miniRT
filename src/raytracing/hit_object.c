ed90988b91613491db234057e6736a3ea8bdbe5a
Shoko Soeno <ssoeno@c5r2s4.42tokyo.jp>
2025-04-29 16:12:07 +0900

    refactored hit_cylinder, modified axix_cylinder.rt

--- src/raytracing/hit_object.c
+++ src/raytracing/hit_object.c
@@ -6,7 +6,7 @@
 /*   By: ssoeno <ssoeno@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 15:45:50 by ssoeno            #+#    #+#             */
-/*   Updated: 2025/04/27 19:38:48 by ssoeno           ###   ########.fr       */
+/*   Updated: 2025/04/29 16:00:49 by ssoeno           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,6 @@
 #include <mlx_util.h>
 #include <raytracing.h>
 
-double	hit_sphere(t_vec center, double radius, t_vec origin, t_vec dir)
-{
-	t_vec	oc;
-	double	a;
-	double	b;
-	double	c;
-	double	discriminant;
-	double	t;
-
-	oc = subtract(origin, center);
-	a = dot_product(dir, dir);
-	b = 2.0 * dot_product(oc, dir);
-	c = dot_product(oc, oc) - radius * radius;
-	discriminant = b * b - 4 * a * c;
-	if (discriminant < 0)
-		return (-1.0);
-	t = (-b - sqrt(discriminant)) / (2.0 * a);
-	if (t < 1e-4)
-	{
-		t = (-b + sqrt(discriminant)) / (2.0 * a);
-		if (t < 1e-4)
-			return (-1.0);
-	}
-	return (t);
-}
-/*
-oc (origin to center)
-*/
 double	hit_plane(t_vec point_on_plane, t_vec normal, t_vec origin, t_vec dir)
 {
 	double	denominator;
@@ -65,33 +37,57 @@ dir: rayの進行方向ベクトル
 denominator: normal vectorとrayの方向が平行に近い = 内積が正 = rayがplaneの裏側から来ている
 */
 
+double solve_quadratic(double a, double b, double c)
+{
+	double	discriminant;
+	double	t;
+
+	discriminant = b * b - 4 * a * c;
+	if (discriminant < 0)
+		return (-1.0);
+	t = (-b - sqrt(discriminant)) / (2.0 * a);
+	if (t < 1e-4)
+	{
+		t = (-b + sqrt(discriminant)) / (2.0 * a);
+		if (t < 1e-4)
+			return (-1.0);
+	}
+	return (t);
+}
+
+double	hit_sphere(t_vec center, double radius, t_vec origin, t_vec dir)
+{
+	t_vec	oc;
+	double	a;
+	double	b;
+	double	c;
+	double	t;
+
+	oc = subtract(origin, center);
+	a = dot_product(dir, dir);
+	b = 2.0 * dot_product(oc, dir);
+	c = dot_product(oc, oc) - radius * radius;
+	t = solve_quadratic(a, b, c);
+	return (t);
+}
+
 double hit_cylinder(t_vec center, double radius, t_vec normal, t_vec origin, t_vec dir)
 {
-    t_vec	origin_to_center;
-    t_vec	ray_dir_perpendicular;
-    t_vec   origin_perpendicular;
+    t_vec	oc;
+    t_vec	ray_perp;
+    t_vec   origin_perp;
     double	a;
     double	b;
     double	c;
-    double	discriminant;
     double	t;
     
-    origin_to_center = subtract(origin, center);
-    ray_dir_perpendicular = subtract(dir, scale(normal, dot_product(dir, normal)));
-    origin_perpendicular = subtract(origin_to_center, scale(normal, dot_product(origin_to_center, normal)));
-    a = dot_product(ray_dir_perpendicular, ray_dir_perpendicular);
-    b = 2.0 * dot_product(ray_dir_perpendicular, origin_perpendicular);
-    c = dot_product(origin_perpendicular, origin_perpendicular) - radius * radius;
-    discriminant = b * b - 4 * a * c;
-    if (discriminant < 0)
-        return (-1.0);
-    t = (-b - sqrt(discriminant)) / (2.0 * a);
-    if (t < 1e-4)
-    {
-        t = (-b + sqrt(discriminant)) / (2.0 * a);
-        if (t < 1e-4)
-            return (-1.0);
-    }
+    oc = subtract(origin, center);
+    ray_perp = subtract(dir, scale(normal, dot_product(dir, normal)));
+    origin_perp = subtract(oc, scale(normal, dot_product(oc, normal)));
+    a = dot_product(ray_perp, ray_perp);
+    b = 2.0 * dot_product(ray_perp, origin_perp);
+    c = dot_product(origin_perp, origin_perp) - radius * radius;
+    t = solve_quadratic(a, b, c);
     return (t);
 }
 
